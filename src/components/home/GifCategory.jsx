0e74c9e0c4de5c9707af012119fce11ec3535eb9
xNatzhu <agustinnahuelsaravia@gmail.com>
2023-06-03 23:27:28 -0300

    update search - axios

--- src/components/home/GifCategory.jsx
+++ src/components/home/GifCategory.jsx
@@ -1,10 +1,23 @@
-
+import axios from "axios"
+import swal from '@sweetalert/with-react'
 
 export default function GifCategory({categorie}){
 
-    const gifFetch = (categorie) =>{
+    useEffect(()=>{
+        const url = "";
         
-    }
+        axios.get(url) 
+        .then( res => {
+        })
+        .catch(error=>{
+            swal({
+                icon:"warning",
+                title: "¡Ups! Hubo un error",
+                text:"Hubo un problema con el servidor, intentelo mas tarde.",
+              });
+        })
+    
+    },[])
 
     return(
      <>
