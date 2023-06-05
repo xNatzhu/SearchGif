851d531ae9f42538bf6fae5497fd644b6cedfb47
xNatzhu <agustinnahuelsaravia@gmail.com>
2023-06-05 17:28:04 -0300

    update component gif Category - Api

--- src/components/home/GifCategory.jsx
+++ src/components/home/GifCategory.jsx
@@ -1,28 +1,49 @@
 import axios from "axios"
-import swal from '@sweetalert/with-react'
+import Swal from 'sweetalert2'
+import { useState, useEffect } from "react"
+
+
+
+
 
 export default function GifCategory({categorie}){
 
+    const [listGif, setListGif] = useState([])
+    const [gifUrl, setGifUrl] = useState(`https://api.giphy.com/v1/gifs/search?api_key=2RtGnE4bNqWnvp3LyrRdh4pmDKb3xlhd&q=${categorie}`)
+
     useEffect(()=>{
-        const url = "";
         
-        axios.get(url) 
+        axios.get(gifUrl) 
         .then( res => {
+             setListGif(res.data.data)
+             console.log(listGif, "Prueba");
         })
         .catch(error=>{
-            swal({
+            Swal({
                 icon:"warning",
                 title: "¡Ups! Hubo un error",
                 text:"Hubo un problema con el servidor, intentelo mas tarde.",
               });
         })
     
-    },[])
+    },[gifUrl]) //cuando la categoria cambie se aplicara nuevo el renderizado
 
     return(
      <>
         <h3>Resultado de busqueda</h3>
         <p>{categorie}</p>
+        <ul>
+            <li>
+                {listGif.map((gif, index)=>{
+                    return(
+                        <div key={index}>
+                            <p>{gif.id}</p>
+                        </div>
+                    )
+                })}
+            </li>
+        </ul>
+
 
      </>   
     )
