17362d742d30a852208491bc737bf4224955ebf0
xNatzhu <agustinnahuelsaravia@gmail.com>
2023-06-10 16:31:02 -0300

    update style img home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -32,7 +32,8 @@ export default function Home() {
         <div className="text-gray-600 bg-gray-200 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4  text-gray-900 font-bold uppercase">Search Gif</h1>
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 text-gray-900 font-bold uppercase bg-gradient-to-r from-indigo-600 via-indigo-700 to-indigo-800 text-transparent bg-clip-text">Search Gif</h1>
+
               <p className="mb-8 leading-relaxed">Explora y descarga contenido visual de manera rápida y sencilla en nuestra plataforma. Encuentra la mejor selección de GIFs, imágenes y stickers para complementar tu contenido.</p>
                 <Buscador addCategory={addCategory}/>
               <p className="text-sm mt-2 text-gray-500 mb-8 w-full">Ejemplo: <span className="text-indigo-500">Anime</span> - Obtendrás un resultado basado en el nombre que ingreses.</p>
@@ -40,7 +41,7 @@ export default function Home() {
               </div>
             </div>
             <div className="hidden lg:max-w-lg lg:w-full md:w-1/2 md:block  w-5/6">
-              <img className="object-cover object-center rounded  shadow-2xl" alt="hero" src="https://wickedblocks.dev/assets/images/placeholders/squareCard.png" /> {/*720x600*/}
+              <img className="object-cover object-center rounded  shadow-2xl transition-transform transform-gpu duration-500 hover:scale-110" alt="hero" src="../public/img/slider.png" /> {/*720x600*/}
             </div>
           </div>
         </div>
